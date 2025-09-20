6a9838c0f2686065773815009fcd6de653839081
Vitor Hugo Fernandes <vitnhmisc@gmail.com>
2025-09-20 15:47:15 -0300

    fix: mobile width improvement

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <div className="bg-gray-800 flex justify-center items-center">
-      <div className="w-full min-h-screen  lg:max-w-[760px] lg:min-w-[760px] min-w-[95vw] px-4 py-4">
+      <div className="w-full min-h-screen  lg:max-w-[760px] lg:min-w-[760px] min-w-[95vw] py-4">
         <LeadsList />
       </div>
     </div>
